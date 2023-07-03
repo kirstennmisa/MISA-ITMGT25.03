466ae14df74b2119cc85358513e2e22c45aafc93
kirstennmisa <135966230+kirstennmisa@users.noreply.github.com>
2023-07-03 13:54:58 +0800

    Add files via upload

---
+++ ipa-1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# Savings
+
+def savings(gross_pay, tax_rate, expenses):
+    
+    output = (int(gross_pay) - (float(tax_rate) * int(gross_pay))) - int(expenses)
+
+    return output
+
+
+# In[4]:
+
+
+# Material Waste
+
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    
+    computation = int(total_material) - (int(num_jobs) * int(job_consumption))
+    output = str(computation) + str(material_units)
+ 
+    return output
+
+
+# In[1]:
+
+
+# Interest
+
+def interest(principal, rate, periods):
+    
+    interest = int(principal) * (float(rate) * int(periods))
+    output = int(principal) + int(interest)
+
+    return output
+
+
+# In[2]:
+
+
+# Body Mass Index
+
+def body_mass_index(weight, height):
+    
+    weight_metric = float(weight) * 0.453592
+    height_metric = (float(height[0]) * 0.3048) + (float(height[1]) * 0.0254)
+    output = float(weight_metric) / (float(height_metric) ** 2)
+
+    return output
+
